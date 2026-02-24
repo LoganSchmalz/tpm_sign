cadc38d91ba232e59e9ceece25e2b3af591f8187
Logan Schmalz <loganschmalz@gmail.com>
2026-02-24 13:16:54 -0600

    Change gen_policies.sh for correct pcrs

--- gen_policies.sh
+++ gen_policies.sh
@@ -15,8 +15,8 @@ tpm2_startauthsession -S session.ctx
 tpm2_policyauthorize -S session.ctx -L authorized.policy -n policy_key.name
 tpm2_flushcontext session.ctx
 # create a PCR policy and sign it
-tpm2_pcrread -opcr0.sha256 sha256:0
+tpm2_pcrread -opcrs.sha256 sha256:7,8,9,11,14
 tpm2_startauthsession -S session.ctx
-tpm2_policypcr -S session.ctx -l sha256:0 -f pcr0.sha256 -L pcr.policy_desired
+tpm2_policypcr -S session.ctx -l sha256:7,8,9,11,14 -f pcrs.sha256 -L pcr.policy_desired
 tpm2_flushcontext session.ctx
 openssl dgst -sha256 -sign policy_key_private.pem -out pcr.signature pcr.policy_desired
